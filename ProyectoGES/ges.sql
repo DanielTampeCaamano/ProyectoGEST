2e94c94dfa1269b80410be3b69c4a1981c055cb4
rubenespinoza1 <44068750+rubenespinoza1@users.noreply.github.com>
2020-08-23 21:34:42 -0400

    Add files via upload

---
+++ ProyectoGES/ges.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-08-2020 a las 03:33:20
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ges`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `monitor`
+--
+
+CREATE TABLE `monitor` (
+  `identificacion` int(10) NOT NULL,
+  `nombreCompleto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `rut` varchar(12) NOT NULL,
+  `nombreCompleto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `patologia`
+--
+
+CREATE TABLE `patologia` (
+  `identificacion` int(10) NOT NULL,
+  `nombrePatologia` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `tipo_persona` varchar(255) DEFAULT NULL,
+  `clave` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `tipo_persona`, `clave`) VALUES
+(201061911, 'registreador1', 'REGISTRADOR', 'clave');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registrador`
+--
+
+CREATE TABLE `registrador` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `registrador`
+--
+
+INSERT INTO `registrador` (`id`, `nombre`) VALUES
+(201061911, 'registreador1');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `monitor`
+--
+ALTER TABLE `monitor`
+  ADD PRIMARY KEY (`identificacion`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`rut`);
+
+--
+-- Indices de la tabla `patologia`
+--
+ALTER TABLE `patologia`
+  ADD PRIMARY KEY (`identificacion`);
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `registrador`
+--
+ALTER TABLE `registrador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `monitor`
+--
+ALTER TABLE `monitor`
+  MODIFY `identificacion` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67807082;
+
+--
+-- AUTO_INCREMENT de la tabla `patologia`
+--
+ALTER TABLE `patologia`
+  MODIFY `identificacion` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registrador`
+--
+ALTER TABLE `registrador`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201061912;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
